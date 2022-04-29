a63d8dc32362fd594a016d65610530ae0ed6523b
ShadowXZT <xiongzhitong@gmail.com>
2022-04-29 10:52:44 +0000

    tbd

---
+++ Dataset4EO/__init__.py
@@ -0,0 +1,2 @@
+#Remote Sensing dataloaders
+import os
---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "Dataset4EO",
+    version = "0.0.1",
+    author = "Zhitong Xiong",
+    author_email = "xiongzhitong@gmail.com",
+    description = ("Dataset loaders for Remote Sensing datasts on Earth observation tasks."),
+    license = "BSD",
+    keywords = "Remote sensing, Datasets, Geospatial",
+    url = "http://packages.python.org/Dataset4EO",
+    packages=['Dataset4EO', 'tests'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
