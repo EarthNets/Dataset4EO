461229fe882f0f8b74111e6a10fb8dde42e1887b
tanmlh <fahongzhang@gmail.com>
2022-05-10 08:43:35 +0000

    test file for landslide4sense

---
+++ test_landslide4sense.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import pdb
+from Dataset4EO.datasets import landslide4sense
+from torch.utils.data import DataLoader2
+import time
+from tqdm import tqdm
+
+datasets_dir = '../../Datasets/Dataset4EO/'
+
+if __name__ == '__main__':
+    # dp = landslide4sense.Landslide4Sense(datasets_dir, split='train')
+    dp = landslide4sense.Landslide4Sense(datasets_dir, split='val')
+
+    data_loader = DataLoader2(dp.shuffle(), batch_size=4, num_workers=1, shuffle=True)
+    for epoch in range(1):
+        t1 = time.time()
+        for it in tqdm(data_loader):
+            # print(it['img_name'])
+            pass
+        t2 = time.time()
+        print(t2-t1)
+
+
