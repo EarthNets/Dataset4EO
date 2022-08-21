05f21200c3fd0ada850c51352f150050cb61f3fb
Fahong Zhang <42575294+tanmlh@users.noreply.github.com>
2022-08-21 09:50:53 +0200

    Update install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,1 +1,6 @@
+pip uninstall torch torchvision torchdata
 pip install --pre torch torchvision torchdata --extra-index-url https://download.pytorch.org/whl/nightly/cu113
+pip install mmcv-full==1.6.0
+pip install prettytable
+pip install pycocotools
+pip install wandb
